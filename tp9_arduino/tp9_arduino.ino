bbf7de5cc6d16ead089104623bb1c8e08ed4b988
chris <cbareil@free.fr>
2014-09-27 12:02:19 +0200

    changement commentaire code ardruino

--- tp9_arduino/tp9_arduino.ino
+++ tp9_arduino/tp9_arduino.ino
@@ -45,10 +45,11 @@ int echoPin = 6;  // pin "echo" branchée dans le 6 de l'Arduino
 // VCC - 5V
 // SDA - ANALOG Pin 4 Uno, Pin 20 Mega
 // SCL - ANALOG pin 5 Uno, Pin 21 Mega
-
+//
 // set the LCD address to 0x27 for a 16 chars 2 line display
 // Set the pins on the I2C chip used for LCD connections: 
 //                    addr, en,rw,rs,d4,d5,d6,d7,bl,blpol
+
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
 
 int ledPin = 13;   // led pin 13
